b2659920215dc6f61fd0f22159dd6c38a1f2cc51
Juan Medina <jamedina@gmail.com>
2017-05-27 20:44:07 +0100

    using the right syntax for Mockito-Kotlin

--- src/test/kotlin/org/learning/by/example/reactive/kotlin/microservices/KotlinReactiveMS/test/TestsExtensions.kt
+++ src/test/kotlin/org/learning/by/example/reactive/kotlin/microservices/KotlinReactiveMS/test/TestsExtensions.kt
@@ -4,10 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.natpryce.hamkrest.Matcher
 import com.nhaarman.mockito_kotlin.*
 import org.learning.by.example.reactive.kotlin.microservices.KotlinReactiveMS.extensions.toMono
-import org.mockito.ArgumentMatcher
-import org.mockito.ArgumentMatchers.anyString
-import org.mockito.internal.matchers.InstanceOf
-import org.mockito.internal.progress.ThreadSafeMockingProgress
 import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.server.EntityResponse
@@ -27,15 +23,6 @@ fun <T : Any> getMonoFromJsonPath(jsonPath: String, type: KClass<T>) = getResour
 fun Any?.isNullValue(): Boolean = this == null
 fun isNull() = Matcher(Any?::isNullValue)
 
-private fun reportMatcher(matcher: ArgumentMatcher<*>) {
-    ThreadSafeMockingProgress.mockingProgress().argumentMatcherStorage.reportMatcher(matcher)
-}
-
-fun anyClass(): Class<Any> {
-    reportMatcher(InstanceOf(Any::class.java, "<any object>"))
-    return Any::class.java
-}
-
 fun <T : Any> mockWebClient(webClient: WebClient, mono: Mono<T>?): WebClient {
 
     val client = spy(webClient)
@@ -44,12 +31,12 @@ fun <T : Any> mockWebClient(webClient: WebClient, mono: Mono<T>?): WebClient {
     doReturn(uriSpec).whenever(client).get()
 
     val headerSpec = mock<WebClient.RequestHeadersSpec<*>>()
-    doReturn(headerSpec).whenever(uriSpec).uri(anyString())
+    doReturn(headerSpec).whenever(uriSpec).uri(any<String>())
     doReturn(headerSpec).whenever(headerSpec).accept(any())
 
     val clientResponse = mock<ClientResponse>()
-    doReturn(mono).whenever(clientResponse).bodyToMono(anyClass())
-    doReturn(Mono.just(clientResponse)).whenever(headerSpec).exchange()
+    doReturn(mono).whenever(clientResponse).bodyToMono(any<Class<Mono<T>>>())
+    doReturn(clientResponse.toMono()).whenever(headerSpec).exchange()
 
     return client
 }
